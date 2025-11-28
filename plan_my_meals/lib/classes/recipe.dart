a20c3839ac9d90f051e31f45fdabc8db948e15c7
Marvin Diedrich <Marvin.Diedrich@schnitzer.eu>
2025-11-28 10:58:22 +0100

    CHANGE: modify / add some fields to recipe class

--- plan_my_meals/lib/classes/recipe.dart
+++ plan_my_meals/lib/classes/recipe.dart
@@ -3,20 +3,28 @@ import '../enums/meal_category.dart';
 
 class Recipe {
   final String name;
+  final int portions;
+  final String instructions;
   final List<Ingredient> ingredients;
-  final double? calories;
-  final double? protein;
-  final double? carbohydrates;
-  final double? fats;
-  final MealCategory? category;
+  final double calories;
+  final double protein;
+  final double carbohydrates;
+  final double fats;
+  final MealCategory category;
+  final String? licenseInfo;
+  final String? source;
 
   Recipe({
     required this.name,
     required this.ingredients,
-    this.calories,
-    this.protein,
-    this.carbohydrates,
-    this.fats,
-    this.category,
+    required this.calories,
+    required this.protein,
+    required this.carbohydrates,
+    required this.fats,
+    required this.category,
+    this.licenseInfo,
+    this.source,
+    required this.portions,
+    required this.instructions,
   });
 }
